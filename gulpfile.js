89424bee09c578c7e6168a0543ef5867d6a08bdb
sarah.geissberger <sarah.geissberger@amazeelabs.com>
2016-02-16 17:55:58 +0100

    AMAGQR-17 sassy- and susyfied project

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,31 @@
-var gulp = require('gulp'),
-    debug = require('gulp-debug');
+var path = {
+    theme:  'www/assets/style/',
+    sass:   'sass',
+    css:    'css',
+    js:     'js',
+    img:    'img'
+};
+
+var gulp          = require('gulp'),
+    util          = require('gulp-util'),
+    sass          = require('gulp-sass'),
+    globbing      = require('gulp-css-globbing'),
+    postcss       = require('gulp-postcss'),
+    autoprefixer  = require('autoprefixer'),
+    browserSync   = require('browser-sync'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    duration      = require('gulp-duration'),
+    filter        = require('gulp-filter'),
+    reload        = browserSync.reload,
+    debug         = require('gulp-debug');
+
+
+/* Set paths */
+path.sass   = path.theme + path.sass;
+path.css    = path.theme + path.css;
+path.js     = path.theme + path.js;
+path.img    = path.theme + path.img;
+
 
 /**
  * Combined Tasks
@@ -59,6 +85,61 @@ gulp.task('browserSync', function() {
       baseDir: 'www'
     }
   });
-  gulp.watch(['app/**/*.js','app/**/*.html', 'assets/**/*.css', 'externals/**/*.js', 'externals/**/*.html','*.html'], {cwd: 'www'}, reload);
+  gulp.watch(['app/**/*.js','app/**/*.html', 'assets/**/*.css','assets/**/*.shelpcss', 'externals/**/*.js', 'externals/**/*.html','*.html'], {cwd: 'www'}, reload);
 });
+
+
+/* Gulp SASS task with browserSync*/
+gulp.task('sass', function () {
+  var processors = [
+    autoprefixer({browsers: ['last 10 versions', 'ie 9']})
+  ];
+  gulp.src(path.sass + '/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(globbing({ extensions: ['.scss'] }))
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(duration('SASS compilation finished'))
+    .pipe(postcss(processors))
+    .pipe(duration('postCSS finished'))
+    .pipe(sourcemaps.write('./map'))
+    .pipe(duration('created sourcemap files'))
+    .pipe(gulp.dest('./' + path.css))
+    .pipe(filter([path.css + '/**/*.css']))
+    .pipe(browserSync.reload({stream: true}))
+    .pipe(filter([path.css + '/**/*.css']))
+    .pipe(duration('moved all files to /css folder'));
+});
+
+/* Gulp SASS prod compile task*/
+gulp.task('sass-build', function () {
+  var processors = [
+    autoprefixer({browsers: ['last 10 versions', 'ie 9']}),
+    csswring
+  ];
+  gulp.src(path.sass + '/**/*.scss')
+    .pipe(globbing({ extensions: ['.scss'] }))
+    .pipe(sass.sync().on('error', sass.logError).on('error', process.exit.bind(process, 1)))
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./' + path.css));
+});
+
+
+/* Gulp compile dev task*/
+gulp.task('compile', ['sass']);
+
+
+/* Gulp build prod task*/
+gulp.task('build', ['sass-build']);
+
+
+/* Gulp browserSync init task*/
+gulp.task('watch', ['browserSync'], function () {
+    gulp.watch(path.sass + '/**/*.scss', ['sass']);
+    gulp.watch(path.js + '/**/*.js').on('change', reload);
+    gulp.watch("*.html").on('change', reload);
+});
+
+
+/* Default task*/
+gulp.task('default', ['watch']);
 
